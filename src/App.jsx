83eb1e4602156b59a874b920eb874d8512d4f2ff
HseinKt <hseinkteish1998@gmail.com>
2025-03-30 05:26:43 +0300

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import FeaturesSection from "./components/FeaturesSection"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
+import './App.css'
 
 function App() {
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,49 @@
+import { Facebook, Instagram, Linkedin, Mail, Phone, Twitter } from 'lucide-react';
+import '../styles/Footer.css'
+
 const Footer = () => {
     return ( 
-        <>Footer</>
+        <footer className='footer'>
+            <div className='container'>
+                <div className='grid'>
+                    <div>
+                        <h3 className='logo'>Hussein Kteish</h3>
+                        <p className='description'>Connecting India through efficient and reliable logistics solutions.</p>   
+                    </div>
+
+                    <div>
+                        <h4 className='title'>Quick Links</h4>
+                        <ul className='links'>
+                            <li><a href='#home' className='link'>Home</a></li>
+                            <li><a href='#features' className='link'>Features</a></li>
+                            <li><a href='#about' className='link'>About</a></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4 className='title'>Contact Us</h4>
+                        <div>
+                            <p className='contactItem'> <Mail className='icon' /> hseinkteish21@gmail.com </p>
+                            <p className='contactItem'> <Phone className='icon' /> +961 76 626 035 </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h4 className='title'>Follow Us</h4>
+                        <div className='socialLinks'>
+                            <a href='#' className='socialLink'  ><Facebook className="icon"/></a>
+                            <a href='#' className='socialLink'  ><Twitter className="icon"/></a>
+                            <a href='#' className='socialLink'  ><Instagram className="icon"/></a>
+                            <a href='#' className='socialLink'  ><Linkedin className="icon"/></a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div className='copyright'>
+                    <p>&copy; Hussein Kteish. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
      );
 }
  
